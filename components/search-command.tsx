112505760d1c7060148a7b41ab883f65f1182c5c
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-10-12 06:52:24 +0200

    fix search

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -65,7 +65,7 @@ export const SearchCommand = () => {
               key={document._id}
               value={`${document._id}-${document.title}`}
               title={document.title}
-              onSelect={onSelect}
+              onSelect={() => onSelect(document._id)}
             >
               {document.icon ? (
                 <p className="mr-2 text-[18px]">
